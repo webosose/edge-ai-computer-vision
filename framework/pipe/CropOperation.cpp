72c1e43781c2e63cf4a4ae166b01061e9c7233de
hayoyo <hayoyo.chang@lge.com>
2023-12-28 16:16:06 +0900

    Add Upscale Factor for rPPG Face Crop
    :Release Notes:
    Add Upscale Factor for rPPG Face Crop
    
    :Detailed Notes:
    - Upsampling scale is fix to 2
    - It works only for rppg_face_crop
    
    :Testing Performed:
    Local Test
    
    :QA Notes:
    N/A
    
    :Issues Addressed:
    [WRP-6600] add upscale factor for rppg face crop
    
    Change-Id: I59d8080a8adecacca750198e0db89b52093e3cc1
    Reviewed-on: http://gpro.lge.com/c/webosose/edge-ai-computer-vision/+/385894
    Reviewed-by: Automated Checker <automated_checker@lge.com>
    Tested-by: Hayoung Chang <hayoyo.chang@lge.com>
    Reviewed-by: Hayoung Chang <hayoyo.chang@lge.com>

--- framework/pipe/CropOperation.cpp
+++ framework/pipe/CropOperation.cpp
@@ -37,7 +37,17 @@ bool CropOperation::runImpl(const std::shared_ptr<NodeInput>& input)
         Loge("crop size : ", cropRect);
         return false;
     }
-    descriptor->setImage(image(cropRect));
+
+    if (m_id.rfind("rppg_face_crop", 0) == 0) {
+        auto cropRect_image = image(cropRect);
+        cv::Mat resizedImg;
+        cv::resize(cropRect_image, resizedImg, cv::Size(), 2, 2, cv::INTER_LINEAR);
+        descriptor->setImage(resizedImg);
+        // imwrite("upScale_face.jpg", descriptor->getImage());
+    }
+    else {
+        descriptor->setImage(image(cropRect));
+    }
 
     rj::Document doc;
     doc.SetObject();
